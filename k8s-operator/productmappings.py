c2720785ffd3c8c29c3cbda3404bbbaafe1df04f
Sharran <Sharran.murali@freshworks.com>
2022-06-11 18:55:51 +0530

    kopf operator

--- k8s-operator/productmappings.py
+++ k8s-operator/productmappings.py
@@ -2,13 +2,13 @@ import kopf
 import logging
 
 
-@kopf.on.create('dashboard.freshworkscorp.com', 'v1', 'productmappings')
+@kopf.on.create('example.com', 'v1', 'productmappings')
 def create_fn(spec, name, **kwargs):
     logging.info(f'Creating product mapping {name}')
     logging.info(f'Spec: {spec}')
     
     
-@kopf.on.update('dashboard.freshworkscorp.com', 'v1', 'productmappings')
+@kopf.on.update('example.com', 'v1', 'productmappings')
 def update_fn(spec, name, **kwargs):
     logging.info(f'Updating product mapping {name}')
     logging.info(f'Spec: {spec}')
