3bdba06d468ca1f67a6116c92700f644d7af6a96
Sharran <Sharran.murali@freshworks.com>
2022-09-20 18:45:09 +0530

    tcp branch

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
-import (
-	"os"
-
-	"github.com/sirupsen/logrus"
-)
-
 func main() {
-	logrus.Info("port: ", os.Getenv("PORT"))
 	svr := NewTCPServer(TCPServerConfig{
 		Host: "localhost",
-		Port: os.Getenv("PORT"),
+		Port: "8080",
 	})
 	svr.ListenAndServe()
 }
