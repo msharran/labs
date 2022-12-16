c6667ef3082740ebcc0f86afe6610cb197ce1626
Sharran <Sharran.murali@freshworks.com>
2022-12-16 17:36:05 +0530

    gorep search pkg

---
+++ go/gorep/internal/search/search_test.go
@@ -0,0 +1,71 @@
+package search
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+const CONTENT = `Lorem ipsum dolor sit amet,
+consectetur adipiscing elit. Curabitur et magna
+vel dolor porttitor ullamcorper vitae non lacus. 
+Etiam consectetur, nibh quis placerat posuere,
+leo eros lacinia massa, eu viverra tellus augue 
+`
+
+func TestSearch(t *testing.T) {
+	testCases := []struct {
+		name string
+		args SearchArgs
+		want string
+	}{
+		{
+			"simple search",
+			SearchArgs{
+				Pattern: "quis",
+			},
+			"Etiam consectetur, nibh quis placerat posuere,\n",
+		},
+		{
+			"search with line numbers",
+			SearchArgs{
+				Pattern:         "Lorem",
+				ShowLineNumbers: true,
+			},
+			"1 Lorem ipsum dolor sit amet,\n",
+		},
+		{
+			"search case insensitive",
+			SearchArgs{
+				Pattern:         "lorem",
+				CaseInsensitive: true,
+			},
+			"Lorem ipsum dolor sit amet,\n",
+		},
+		{
+			"search line count",
+			SearchArgs{
+				Pattern:       "et",
+				ShowLineCount: true,
+			},
+			"3\n",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			w := &bytes.Buffer{}
+
+			err := Search(w, strings.NewReader(CONTENT), tc.args)
+
+			if err != nil {
+				t.Error(err)
+			}
+
+			if w.String() != tc.want {
+				t.Errorf("got: %q want: %q", w.String(), tc.want)
+			}
+		})
+	}
+}
