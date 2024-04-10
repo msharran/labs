4b49802dc806dbcfe68155f4586bb35f2f56f868
Sharran <sharran.murali@gmail.com>
2024-04-10 23:44:18 +0530

    htmx kv store

--- go-htmx-kvstore/main.go
+++ go-htmx-kvstore/main.go
@@ -233,58 +233,5 @@ func main() {
 		return c.NoContent(200)
 	})
 
-	// e.POST("/start-session", func(c echo.Context) error {
-	// 	username := c.FormValue("username")
-	//
-	// 	_, exists := registeredUsers[username]
-	// 	if !exists {
-	// 		registeredUsers[username] = &UserData{}
-	// 	}
-	//
-	// 	e.Logger.Infof("user=%s; new=%v\n", username, !exists)
-	//
-	// 	// https://htmx.org/essays/web-security-basics-with-htmx/#secure-your-cookies
-	// 	// Set-Cookie header instructs browser to send cookie in subsequent requests
-	// 	cookie := new(http.Cookie)
-	// 	cookie.Name = "username"
-	// 	cookie.Value = username
-	// 	cookie.HttpOnly = true
-	// 	c.SetCookie(cookie)
-	//
-	// 	return c.Render(200, "index.html", echo.Map{
-	// 		"Title":           "Home",
-	// 		"Username":        username,
-	// 		"RegisteredUsers": getRegisteredUsers(),
-	// 	})
-	// })
-	//
-	// e.DELETE("/close-session", func(c echo.Context) error {
-	//
-	// 	user, err := c.Cookie("username")
-	// 	if err != nil {
-	// 		return c.NoContent(http.StatusInternalServerError)
-	// 	}
-	// 	delete(registeredUsers, user.Value)
-	//
-	// 	// delete cookie and return HX header to refresh page
-	// 	cookie := new(http.Cookie)
-	// 	cookie.Name = "username"
-	// 	cookie.Value = ""
-	// 	cookie.HttpOnly = true
-	// 	cookie.MaxAge = -1
-	// 	c.SetCookie(cookie)
-	//
-	// 	c.Response().Header().Set("HX-Refresh", "true")
-	// 	return c.NoContent(http.StatusNoContent)
-	// })
-
 	e.Logger.Fatal(e.Start("localhost:1323"))
 }
-
-// func getRegisteredUsers() []string {
-// 	users := make([]string, 0, len(registeredUsers))
-// 	for user := range registeredUsers {
-// 		users = append(users, user)
-// 	}
-// 	return users
-// }
