39b956aa2174dad8b5e0411b480eeb03621396f5
Sharran <sharran.murali@gmail.com>
2024-05-14 23:28:25 +0530

    restructure

--- rust-cargo/target/debug/deps/rust_cargo-ccdfc16d5edca771.d
+++
@@ -1,5 +0,0 @@
-/Users/shmurali/play/labs/rust-cargo/target/debug/deps/rust_cargo-ccdfc16d5edca771.rmeta: src/main.rs
-
-/Users/shmurali/play/labs/rust-cargo/target/debug/deps/rust_cargo-ccdfc16d5edca771.d: src/main.rs
-
-src/main.rs:
