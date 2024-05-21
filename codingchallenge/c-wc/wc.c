a8d3ebe0c830bea204399244bb3da96e07227141
Sharran <sharran.murali@gmail.com>
2024-05-22 00:36:15 +0530

    wc - char count

--- codingchallenge/c-wc/wc.c
+++ codingchallenge/c-wc/wc.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
-int main() {
-  printf("Hello, World!\n");
-  return 0;
+#include <string.h>
+
+int get_char_c(FILE *f) {
+    int count = 0;
+    char c;
+    while ((c = fgetc(f)) != EOF) {
+        count++;
+    }
+    return count;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("wc: missing args -- got=%d, want=%d\n", argc, 3);
+        fprintf(stderr,"usage: wc [-c] [file ...]\n");
+        return 1;
+    }
+
+    char *flag = argv[1];
+    char *file = argv[2];
+
+    FILE *f = fopen(file, "r");
+    if (f == NULL) {
+        perror("fopen");
+        return 1;
+    }
+
+    if (strcmp(flag, "-c") == 0) {
+        int cc = get_char_c(f);
+        printf("      %d %s\n", cc, file);
+    }
+
+    int retval = fclose(f);
+    if (retval != 0){
+        perror("fclose");
+        return retval;
+    }
+    return 0;
 }
