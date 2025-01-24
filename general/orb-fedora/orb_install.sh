7f3919f3502329fce9bcb14b94bb2198f55253b1
Sharran <sharran.murali@gmail.com>
2025-01-24 17:43:52 +0530

    Orb installer

--- general/orb-fedora/orb_install.sh
+++ general/orb-fedora/orb_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-orbctl -m fedora push install.sh /home/msharran/install.sh
-orbctl -m fedora run bash /home/msharran/install.sh
+orbctl -m fedora push installer.sh /home/msharran/installer.sh
+orbctl -m fedora run bash /home/msharran/installer.sh
