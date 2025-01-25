928fd61c4d7836cfa6bfcddaad8a458212e89505
Sharran <sharran.murali@gmail.com>
2025-01-25 16:37:30 +0530

    Install dev tools and zig 0.14

--- general/fedora-vm/installer.sh
+++ general/fedora-vm/installer.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-run_cmd() {
-    eval $@
-    if $? -ne 0; then 
-        exit 1
-    fi
-}
+set -e
 
 echo "*Installing dependencies"
 sudo dnf -y update
-sudo dnf -y groupinstall "Development Tools"
+sudo dnf -y group install workstation-product-environment
 sudo dnf install -y git\
     bat\
     fzf\
@@ -19,17 +14,30 @@ sudo dnf install -y git\
     neovim\
     make\
     curl\
-    zig\
     golang\
     stow\
     fish\
     btop\
     redis
 
+ZIG_VERSION=0.14.0-dev.2606+b039a8b61
+echo "*Installing zig $ZIG_VERSION"
+
+pushd /opt \
+    && wget https://ziglang.org/builds/zig-linux-aarch64-${ZIG_VERSION}.tar.xz \
+    && tar -xf zig-linux-aarch64-${ZIG_VERSION}.tar.xz \
+    && ln -s /opt/zig-linux-aarch64-${ZIG_VERSION}/zig /usr/local/bin/zig \
+    && rm zig-linux-aarch64-${ZIG_VERSION}.tar.xz \
+    && zig version
+    && popd
+
 echo "*Installing dotfiles"
 git clone https://github.com/msharran/.dotfiles /home/msharran/.dotfiles
 cd /home/msharran/.dotfiles
 make
+
+echo "*Installing nvim Plugins"
+nvim --headless +PlugInstall +qall
 
 echo "*Installing starship"
 curl -sS https://starship.rs/install.sh | sh
