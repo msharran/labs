f7cd81dd8b16bc7594be9b3c01fa75555b5a25b0
Sharran <sharran.murali@gmail.com>
2025-01-31 18:53:24 +0000

    vm installer

--- vm/bootstrap.sh
+++ vm/bootstrap.sh
@@ -24,10 +24,10 @@ ZIG_VERSION=0.14.0-dev.2606+b039a8b61
 echo "*Installing zig $ZIG_VERSION"
 
 pushd $HOME/.local
-    wget https://ziglang.org/builds/zig-linux-aarch64-${ZIG_VERSION}.tar.xz 
-    tar -xf zig-linux-aarch64-${ZIG_VERSION}.tar.xz 
+    wget https://ziglang.org/builds/zig-linux-aarch64-${ZIG_VERSION}.tar.xz
+    tar -xf zig-linux-aarch64-${ZIG_VERSION}.tar.xz
     sudo ln -sf $HOME/.local/zig-linux-aarch64-${ZIG_VERSION}/zig /usr/local/bin/zig
-    rm zig-linux-aarch64-${ZIG_VERSION}.tar.xz 
+    rm zig-linux-aarch64-${ZIG_VERSION}.tar.xz
     zig version
 popd
 
@@ -36,3 +36,11 @@ curl -sS https://starship.rs/install.sh | sh
 
 echo "*Setting fish as default shell"
 sudo chsh -s /usr/bin/fish msharran
+
+echo "*Installing dotfiles"
+if [ ! -d "$HOME/.dotfiles" ]; then
+    git clone https://github.com/msharran/.dotfiles $HOME/.dotfiles
+    pushd $HOME/.dotfiles
+        make
+    popd
+fi
