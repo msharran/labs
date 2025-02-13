9fba83f90eba2beb2243e4ddf2607cbeaaa6f7a7
Sharran <sharran.murali@gmail.com>
2025-02-13 17:45:26 +0000

    pulumi examples

--- general/pulumi/k8s-microservice-service-module/test/curl_jenkins_agent.sh
+++ general/pulumi/k8s-microservice-service-module/test/curl_jenkins_agent.sh
@@ -1,3 +1,1 @@
-set -x
-
 curl -H "Host: jenkins-agent.example.com" http://172.18.0.2:30587/ | w3m -dump -T text/html
--- general/pulumi/k8s-microservice-service-module/test/curl_jenkins_master.sh
+++ general/pulumi/k8s-microservice-service-module/test/curl_jenkins_master.sh
@@ -1,3 +1,1 @@
-set -x
-
 curl -H "Host: jenkins.example.com" http://172.18.0.2:30587/login | w3m -dump -T text/html
